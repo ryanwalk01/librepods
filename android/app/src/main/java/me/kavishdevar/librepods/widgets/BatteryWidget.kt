e558f0a72cfb5b5b9d550a2063d7ddba1006e1ba
ryanwalk01 <ryan.walk01@gmail.com>
2026-03-30 17:45:11 -0400

    Fixed widget: package actually on top

--- android/app/src/main/java/me/kavishdevar/librepods/widgets/BatteryWidget.kt
+++ android/app/src/main/java/me/kavishdevar/librepods/widgets/BatteryWidget.kt
@@ -15,8 +15,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-import android.content.Intent
-
 package me.kavishdevar.librepods.widgets
 
 @file:OptIn(ExperimentalEncodingApi::class)
@@ -26,6 +24,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import me.kavishdevar.librepods.services.ServiceManager
 import kotlin.io.encoding.ExperimentalEncodingApi
+import android.content.Intent
 
 class BatteryWidget : AppWidgetProvider() {
     override fun onUpdate(
